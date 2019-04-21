daddf92083512d0a3739be2fbd2b3c5e3f4413a2
Cody Bloemhard <cblmhrd@live.nl>
2019-04-21 15:10:57 +0100

    darker bg

--- config.h
+++ config.h
@@ -85,7 +85,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 4;
 
 //background opacity
-unsigned int alpha = 0x9c;
+unsigned int alpha = 0xaa;//9c
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
